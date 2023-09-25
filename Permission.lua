71887925dea8401793874c02bd16426f9a7cf61c
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-09-25 03:52:01 -0300

    Create Permission.lua

---
+++ Permission.lua
@@ -0,0 +1,31 @@
+local Tabela = {
+	PlayerPermission = {
+		["Fonojonzo"] = 			{Tier = 1000},
+		["ExTnoCraK"] = 			{Tier = 1000},
+		["VigorOGr"] = 				{Tier = 1000},
+		["jguuh"] = 				{Tier = 1},
+		["b112014"] = 				{Tier = 1},
+		["RaihNoCraK"] = 			{Tier = 1},
+		["zazax1120"] = 			{Tier = 1},
+		["ProzinhoBr23213"] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Locked = {785068132,36651812}},
+		["DIO"]	=					{Locked = {36651812}},
+		["Madara"]	=				{Locked = {36651812,785068132}},
+		["Koku"]	=				{Locked = {785068132}},
+		["Yori"]	=				{Locked = {36651812}},
+		["Meliodas"]	=				{Locked = {629259254}},
+		["Goku"]	=				{Locked = {785068132,327518760}},
+		["TOP"]	=					{Locked = {}},
+		["Giorno"] = 					{Locked = {36651812,785068132,629259254,13032042}},
+		["Hakaishin"] = 				{Locked = {36651812,785068132,629259254,13032042}},
+		["Boss"] = 					{Locked = {785068132,36651812}},
+		["JP6"] = 					{Tier = 1,Permission = {1175114342,2908376678,1706565385}},
+		["Gojo"] = 					{Tier = 1,Permission = {4633711075,2582436510,4386076823,1525050995,2929532380,2963030044,2908376678,3108673472}}, -- perm temp
+	}
+
+}
+
+return Tabela
