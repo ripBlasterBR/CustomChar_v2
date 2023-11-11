b0252c81167fdd3ed4b167127fd7df73401e2d21
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-11-11 17:57:13 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -14,6 +14,7 @@ local Tabela = {
 		["GG_TIAGO100"] = 			{Tier = 1},
 		["FerPro_13"] = 			{Tier = 1},
 		["FaustinoCB"] = 			{Tier = 1},
+		["Bloxfruits3y7"] = 			{Tier = 1},
 	},
 	
 	CharacterTier = {
