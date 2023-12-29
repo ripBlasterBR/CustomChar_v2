dc76552466fce88d13eceecbba0152a830b0d25e
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-12-28 23:43:35 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -27,7 +27,9 @@ local Tabela = {
 		["xandecat11"] = 			{Tier = 1}, --  comprou gojo	
 		["Dells_lakash"] = 			{Tier = 1}, --  comprou gojo	
 		["Kim_lola123"] = 			{Tier = 1}, --  amiga da isa (10 tickets de venda)
-		["GamerLivegameplays"] = 		{Tier = 1}, --  amiga da isa (10 tickets de venda)
+		["GamerLivegameplays"] = 		{Tier = 1}, --  isa (arrumar sistema de permission e tirar gojo (otaria)
+		["A_Menor01"] = 			{Tier = 1}, --  comprou gojo	
+		
 	},
 	
 	CharacterTier = {
