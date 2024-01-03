735cee95b28d6337f5b6b9fa0b9bad13d9b2d5d1
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-01-03 05:56:59 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -1,6 +1,6 @@
 local Tabela = {
 	PlayerPermission = {
-		["Fonojonzo"] = 			{Tier = 1},
+		--["Fonojonzo"] = 			{Tier = 1},
 		["ExTnoCraK"] = 			{Tier = 1000},
 		["VigorOGr"] = 				{Tier = 1000},
 		["jguuh"] = 				{Tier = 1},
@@ -14,38 +14,22 @@ local Tabela = {
 		["GG_TIAGO100"] = 			{Tier = 1},
 		["FerPro_13"] = 			{Tier = 1},
 		["FaustinoCB"] = 			{Tier = 1},
-		["Sr_samuel099"] = 			{Tier = 1}, -- Comprou gojo
-		["pedrinhouuuuuuu"] = 			{Tier = 1}, -- Comprou Gojo
-		["kelvynk93"] = 			{Tier = 1}, -- Comprou Jotaro v2 100 mas não tem mobile
-		["darkzinhoUAS"] = 			{Tier = 1}, -- Comprou o Rimuru (dar gojo enquanto crio Rimuru / deu 50 pra emprestar o gojo)
 		["ltz_Lufe"] = 				{Tier = 1}, -- c
 		["srnubaso"] = 				{Tier = 1}, -- nubaseo
-		["luffyzin_rosquinhace"] = 		{Tier = 1}, --  comprou (100) gojo
-		["zRn7_GolD"] = 			{Tier = 1}, --  comprou (100) gojo
-		["KozierNoob123"] = 			{Tier = 1}, --  comprou (100) gojo
-		["guiccm4d8"] = 			{Tier = 1}, --  comprou (130) gojo
-		["GojonoCB"] = 				{Tier = 1}, --  comprou (200) gojo v2/ remover ao lançar gojo v2	
-		["xandecat11"] = 			{Tier = 1}, --  comprou gojo	
-		["Dells_lakash"] = 			{Tier = 1}, --  comprou gojo	
-		["Kim_lola123"] = 			{Tier = 1}, --  amiga da isa (10 tickets de venda)
-		["GamerLivegameplays"] = 		{Tier = 1}, --  isa (arrumar sistema de permission e tirar gojo (otaria)
-		["A_Menor01"] = 			{Tier = 1}, --  comprou gojo	
-		["gustavosmf"] = 			{Tier = 1}, --  comprou gojo	
-		["coelhocaralimpa"] = 			{Tier = 1}, --  comprou gojo	
-		["nahqi88iqu"] =  			{Tier = 1}, --  comprou gojo	
-		
-		
 	},
 	
 	CharacterTier = {
-		["gojo"] 	= 				{Tier = 1,Permission = {4633711075,4386076823,1525050995,2908376678,3108673472,2386263216,375521456}},
+		["gojo"] 	= 				{Tier = 1,Permission = {4633711075,4386076823,1525050995,2908376678,3108673472,2386263216,375521456,
+				4776221194,1204922626,375521456,1502674877,2963030044,4035850120,1513291266,3667599085,4645701612,3947573158,3760549720,3251265663,
+				914914325,
+			}},
 		["sukuna"]	=				{Locked = {36651812}},
 		["goku"]	=				{Locked = {4633711075,36651812,2386263216,128677359}},
-		["jp6"]		=				{Locked = {4633711075,36651812,2386263216,128677359,834610807,1793600541}},
+		["jp6"]		=				{Locked = {4633711075,36651812,2386263216,128677359,834610807,1793600541,834610807,3162059849}},
 		["rimuru"]	=				{Locked = {3337736146}},
 		["nakime"]	=				{Locked = {2963030044}},
 		["gyutaro"]	=				{Locked = {4633711075}},
-		["gojov2"]	=				{Locked = {36651812}},
+		["gojov2"]	=				{Locked = {36651812,3667599085}},
 		
 	}
 
