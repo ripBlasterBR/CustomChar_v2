b4c75dc1e8e7bdd98c275636969e3dea060ea1e2
iExTno <132175289+iExTno@users.noreply.github.com>
2024-03-04 12:05:13 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -8,7 +8,6 @@
 		["b112014"] = 				{Tier = 1},
 		["ynxvmp"] = 				{Tier = 1},
 		["zazax1120"] = 			{Tier = 1},
-		["OpeTrafalgar"] =			{Tier = 777777},
 		["ProzinhoBr23213"] = 			{Tier = 1},		
 		["Renan2809"] = 			{Tier = 777777},
 		["NuelNoCra"] = 			{Tier = 2},
